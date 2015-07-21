ec871f955233ac6de488e054c162e08080f54b78
Simon Urli <simon.urli@gmail.com>
2015-07-21 10:42:10 +0200

    SERVICES-26 Initialize first source

---
+++ scripts/CalendarNamespaceManager.ts
@@ -0,0 +1,20 @@
+/**
+ * @author Simon Urli <simon@the6thscreen.fr>
+ */
+
+/// <reference path="../t6s-core/core-backend/scripts/server/SourceNamespaceManager.ts" />
+
+class CalendarNamespaceManager extends SourceNamespaceManager {
+
+	/**
+	 * Constructor.
+	 *
+	 * @constructor
+	 * @param {any} socket - The socket.
+	 */
+	constructor(socket : any) {
+		super(socket);
+
+		this.addListenerToSocket("NextXHoursEvents", NextXHoursEvents.apply);
+	}
+}
